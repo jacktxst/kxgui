86a93e4e9900e3fddc6d2b91984d5c31b672fe78
jacktxst <jacklathamlewis@gmail.com>
2026-03-13 22:21:32 -0500

    moved stuff around yet again

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,1 +1,1 @@
-emcc main.c -I . -I sokol -I kxgui -s USE_WEBGL2=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1 -DSOKOL_GLES3 -DSOKOL_IMPL --shell-file shell.html -o docs/wasm-demo.html
+emcc sokol_example_main.c -I kxcore -I . -I sokol -I kxgui -s USE_WEBGL2=1 -s FULL_ES3=1 -s ALLOW_MEMORY_GROWTH=1 -DSOKOL_GLES3 -DSOKOL_IMPL --shell-file shell.html -o docs/wasm-demo.html
